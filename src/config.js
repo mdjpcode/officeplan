219a3569f4a6c3e7bfd4e08f44c40e3655378a61
mdjpcode <gv8849fc6j@privaterelay.appleid.com>
2026-04-29 20:42:56 +0100

    Remove npm cache setting requiring lockfile in CI

---
+++ src/config.js
@@ -0,0 +1,25 @@
+export const START_DATE = '2026-05-01';
+
+export const STAFF = [
+  'Akutekwe', 'Ankers', 'Brownlow', 'Cherryman', 'Clark', 'Corrigan', 'Cruddas',
+  'Di Domenico', 'Fleming', 'Graham-Ozanne', 'Hassall', 'Hesketh', 'Hill', 'Hughes',
+  'Kamran', 'Karlovich', 'Kidambi', 'Kingsmith', 'Kingston', 'Knox', 'Lawton', 'Lord',
+  'Marshall', 'Milner', 'Munn', 'Power', 'Schreiber', 'Sinclair', 'Smith', 'Taylor',
+  'Trodden', 'Urquhart', 'Watson', 'Williams', 'Workman'
+];
+
+export const ROOMS = [
+  { name: 'Mezz/CICA', capacity: 24 },
+  { name: 'Lovelace/Schro.', capacity: 16 },
+  { name: 'Kennedy House', capacity: 15 },
+  { name: 'Top floor', capacity: 16 },
+  { name: 'Heisenberg', capacity: 8 },
+  { name: 'Repo', capacity: 5 },
+  { name: 'Darwin', capacity: 5 },
+  { name: 'CTS', capacity: 4 }
+];
+
+export const VIRTUAL_LOCATIONS = [
+  { name: 'Remote', capacity: null },
+  { name: 'Client Site', capacity: null }
+];
